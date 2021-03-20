86a5578f7410c7501a60513bfc0d415ffea0ff51
Aimen-Allah Ftaiti <55765272+aimenftaiti@users.noreply.github.com>
2021-03-20 23:01:06 +0100

    added tests - update github actions

--- api/app.py
+++ api/app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
 from api.config import env_config
 api = Api()
+jwt = JWTManager()
 def create_app(config_name):
     import resources
     app = Flask(__name__)
     app.config.from_object(env_config[config_name])
     api.init_app(app)    
+    jwt.init_app(app)
     return app

da2131b9163f9eb939a434f664049a654d948a7c
Aimen-Allah Ftaiti <55765272+aimenftaiti@users.noreply.github.com>
2021-03-20 10:01:31 +0100

    fixed untracked files

--- resources/default.py
+++ resources/default.py
@@ -1,19 +1,13 @@
-from flask_restful import Resource, request
-from flask import jsonify, send_file
-from pytube import YouTube
+from flask_restful import Resource
 from api.app import api
-
-class YoutubeResource(Resource):
+class DefaultResource(Resource):
     """Handle default route."""
     def get(self):
         """Get request for home page or response."""
-        args = request.args
-        url = args['url']
-
-        video = YouTube(url)
-        stream = video.streams.get_highest_resolution()
-        stream.download()
-
-        return send_file(stream.download(), as_attachment=True)
-
-api.add_resource(YoutubeResource, "/api/dl", endpoint="home")
+        return {
+            "status": "success",
+            "data": {
+                "msg": "Welcome to our diary API"
+            }
+        }
+api.add_resource(DefaultResource, "/api", endpoint="home")
